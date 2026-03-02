1517f4e1673a5fa12c6ae5059b93d1b0d862d297
jacksonks <jkdsgg@gmail.com>
2026-03-02 06:10:07 -0300

    component ProjectsCard

--- src/App.vue
+++ src/App.vue
@@ -25,27 +25,6 @@ export default {
   <v-app>
     <v-app-bar app color="black" dark>
       <v-toolbar-title>
-        <!--
-        <div class="d-flex align-center">
-          <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-            transition="scale-transition"
-            width="40"
-          />
-
-          <v-img
-            alt="Vuetify Name"
-            class="shrink mt-1 hidden-sm-and-down"
-            contain
-            min-width="100"
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-            width="100"
-          />
-        </div>
--->
         <div class="word d-flex align-center">
           <span class="letter">P</span>
           <span class="letter">O</span>
--- src/components/Projects/Projects.vue
+++ src/components/Projects/Projects.vue
@@ -1,8 +1,10 @@
 <script>
 import projects from "@/assets/projects.json";
+import ProjectsCard from "@/components/Projects/ProjectsCard.vue";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Projects",
+  components: { ProjectsCard },
   data() {
     return {
       overlay: false,
@@ -63,13 +65,6 @@ export default {
         <span class="display-1">projetos</span>
       </v-col>
       <v-col cols="12" md="3">
-        <!--        <v-combobox-->
-        <!--        <v-select-->
-        <!--        <v-autocomplete-->
-        <!--        single-line
-        hide-details
-        clearable
-        dense-->
         <v-autocomplete
           v-model="option"
           :items="options"
@@ -83,14 +78,6 @@ export default {
           clearable
           @change="filterProjects"
         >
-          <!--          <template v-slot:no-data>
-            <v-list-item>
-              <v-list-item-title>
-                Search for your favorite
-                <strong>Cryptocurrency</strong>
-              </v-list-item-title>
-            </v-list-item>
-          </template>-->
           <template v-slot:selection="data">
             <v-chip
               :key="JSON.stringify(data.item)"
@@ -154,219 +141,8 @@ export default {
           </v-col>
         </v-row>
       </div>
-      <div class="pa-3" v-for="(item, i) in projects" :key="i">
-        <v-hover>
-          <template v-slot:default="{ hover }">
-            <v-card
-              :elevation="hover ? 24 : 4"
-              class="mx-auto"
-              max-width="350px"
-              min-width="350px"
-              min-height="550px"
-              max-height="550px"
-            >
-              <div v-if="item.video">
-                <video
-                  height="200"
-                  width="350"
-                  controls
-                  controlsList="nodownload"
-                  autoplay
-                  loop
-                  muted
-                  poster
-                >
-                  <source :src="item.video" type="video/mp4" />
-                  Seu navegador não suporta a tag de vídeo.
-                </video>
-              </div>
-              <div v-else-if="item.img">
-                <v-img
-                  v-if="!hover"
-                  contain
-                  min-height="200px"
-                  max-height="200px"
-                  :src="item.img"
-                  :lazy-src="item.img"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                >
-                  <template v-slot:placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular
-                        indeterminate
-                        color="grey lighten-5"
-                      ></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-                <v-img
-                  v-else
-                  min-height="200px"
-                  max-height="200px"
-                  :src="item.img"
-                  :lazy-src="item.img"
-                  aspect-ratio="10"
-                  class="grey lighten-2"
-                >
-                  <template v-slot:placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular
-                        indeterminate
-                        color="grey lighten-5"
-                      ></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-              </div>
-              <div v-else-if="!item.img">
-                <v-img
-                  v-if="!hover"
-                  contain
-                  min-height="200px"
-                  max-height="200px"
-                  :src="item.imgs[0].src"
-                  :lazy-src="item.imgs[0].src"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                >
-                  <template v-slot:placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular
-                        indeterminate
-                        color="grey lighten-5"
-                      ></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-                <v-img
-                  v-else
-                  contain
-                  min-height="200px"
-                  max-height="200px"
-                  :src="item.imgs[1].src"
-                  :lazy-src="item.imgs[1].src"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                >
-                  <template v-slot:placeholder>
-                    <v-row
-                      class="fill-height ma-0"
-                      align="center"
-                      justify="center"
-                    >
-                      <v-progress-circular
-                        indeterminate
-                        color="grey lighten-5"
-                      ></v-progress-circular>
-                    </v-row>
-                  </template>
-                </v-img>
-              </div>
-              <v-card-title
-                class="primary--text text-center align-center justify-center"
-              >
-                {{ item.title }}
-              </v-card-title>
-              <v-card-subtitle>
-                <v-row align="center" justify="center" text="center">
-                  <div v-for="(icon, key) in item.skills" :key="key">
-                    <v-tooltip bottom>
-                      <template v-slot:activator="{ on, attrs }">
-                        <img
-                          v-bind="attrs"
-                          v-on="on"
-                          class="mr-1"
-                          height="25px"
-                          :src="
-                            'https://skillicons.dev/icons?i=' +
-                            icon +
-                            '&theme=light'
-                          "
-                          :alt="icon"
-                        />
-                      </template>
-                      <span>{{ icon }}</span>
-                    </v-tooltip>
-                  </div>
-                </v-row>
-              </v-card-subtitle>
-              <v-card-text class="text-justify" style="min-height: 214px">
-                <span>{{ item.text }}</span>
-              </v-card-text>
-              <v-card-actions class="d-flex justify-space-between">
-                <v-btn
-                  v-if="item.link"
-                  color="primary"
-                  dark
-                  :href="item.link"
-                  target="_blank"
-                  small
-                >
-                  Visualizar
-                  <v-icon left>mdi-open-in-new</v-icon>
-                </v-btn>
-                <v-btn
-                  v-if="item.android"
-                  color="green"
-                  dark
-                  :href="item.link"
-                  target="_blank"
-                  fab
-                  text
-                  small
-                >
-                  <v-icon>mdi-android</v-icon>
-                </v-btn>
-                <v-btn
-                  v-if="item.ios"
-                  color="grey"
-                  dark
-                  :href="item.link"
-                  target="_blank"
-                  fab
-                  text
-                  small
-                >
-                  <v-icon>mdi-apple</v-icon>
-                </v-btn>
-                <v-btn v-if="item.id === 6" color="black" dark fab text small>
-                  <v-icon>mdi-cellphone</v-icon>
-                </v-btn>
-                <v-btn v-if="item.id === 6" color="black" dark fab text small>
-                  <v-icon>mdi-tablet</v-icon>
-                </v-btn>
-                <v-btn v-if="item.id === 6" color="black" dark fab text small>
-                  <v-icon>mdi-laptop</v-icon>
-                </v-btn>
-                <v-btn
-                  v-if="item.code"
-                  color="black"
-                  :href="item.code"
-                  target="_blank"
-                  dark
-                  fab
-                  text
-                  small
-                >
-                  <v-icon>mdi-github</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-hover>
+      <div class="pa-3" v-for="(project, i) in projects" :key="i">
+        <projects-card :item="project"></projects-card>
       </div>
     </v-row>
   </v-container>
